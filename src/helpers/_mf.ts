aa71a0921ae58a33892037b8091b835aaf6c3207
Tyler van der Hoeven <hi@tyvdh.com>
2022-07-01 20:31:30 -0400

    fixed some issues with env files

--- src/helpers/_mf.ts
+++ src/helpers/_mf.ts
@@ -52,7 +52,7 @@ export default async (_platform: App.Platform) => {
   const { Miniflare } = await import('miniflare')
   const mf = new Miniflare({
     modules: true,
-    envPath: './.env.local',
+    envPath: './.env.development.local',
     packagePath: true,
     wranglerConfigPath: './wrangler-dev.toml',
 
